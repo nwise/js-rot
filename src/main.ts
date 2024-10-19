0e5886052c5cbd2e0c8a9b935870b539be143447
Nathan Wise <nathanwise@gmail.com>
2024-10-19 16:55:35 -0400

    Updating player background color

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ declare global {
 
 window.addEventListener('DOMContentLoaded', () => {
   const npc = new Entity(Engine.WIDTH / 2 - 5, Engine.HEIGHT / 2, '@', '#ff0');
-  const player = new Entity(Engine.WIDTH / 2, Engine.HEIGHT / 2, '@',);
+  const player = new Entity(Engine.WIDTH / 2, Engine.HEIGHT / 2, '@', '#fff', '#c8b432');
   const entities = [npc, player];
   window.engine = new Engine(entities, player);
 })
