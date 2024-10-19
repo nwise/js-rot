5aec00c5fffe43574d2deb6ed41326c06e9006ac
Nathan Wise <nathanwise@gmail.com>
2024-10-19 16:47:02 -0400

    FOV built

--- src/tile-types.ts
+++ src/tile-types.ts
@@ -7,17 +7,26 @@ export interface Graphic {
 export interface Tile {
   walkable: boolean;
   transparent: boolean;
+  visible: boolean;
+  seen: boolean;
   dark: Graphic;
+  light: Graphic;
 }
 
 export const FLOOR_TILE: Tile = {
   walkable: true,
   transparent: true,
-  dark: { char: ' ', fg: '#fff', bg: '#323296' }
+  visible: false,
+  seen: false,
+  dark: { char: ' ', fg: '#fff', bg: '#323296' },
+  light: { char: ' ', fg: '#fff', bg: '#c8b432' }
 }
 
 export const WALL_TILE: Tile = {
   walkable: false,
   transparent: false,
-  dark: { char: ' ', fg: '#fff', bg: '#000064' }
+  visible: false,
+  seen: false,
+  dark: { char: ' ', fg: '#fff', bg: '#000064' },
+  light: { char: ' ', fg: '#fff', bg: '#826e32' }
 }
