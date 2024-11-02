86be81e2a9ca93c001b6e404d7b1f44001c1242f
Nathan Wise <nathanwise@gmail.com>
2024-11-02 11:14:44 -0400

    Adding Health bar

---
+++ src/colors.ts
@@ -0,0 +1,11 @@
+export enum Colors {
+  White = '#ffffff',
+  Black = '#000000',
+  PlayerAttack = '#e0e0e0',
+  EnemyAttack = '#ffc0co',
+  PlayerDie = '#ff3030',
+  EnemeyAttack = '#ffa030',
+  WelcomeText = '#20a0ff',
+  BarFilled = '#006000',
+  BarEmpty = '#041010a'
+}
